f5a7002cdd5750f6a073451f6256bd3f0d9dc9be
JoungMinJu <alswn9938@gmail.com>
2021-10-27 20:04:08 +0900

    [21.10.27 정민주][Mod] PEP8 코드 수정

--- src/dino.py
+++ src/dino.py
@@ -1,42 +1,56 @@
 from src.setting import *
+import pygame
+from pygame import mixer
 
 
 class Dino():
     def __init__(self, sizex=-1, sizey=-1,type = None):
-        
-        # 디노의 타입을 결정합니다. 
         self.type = type
-
         if type == 'ORIGINAL':
-            self.images, self.rect = load_sprite_sheet('dino.png', 6, 1, sizex, sizey, -1)
-            # self.images, self.rect = load_sprite_sheet('pinkdino.png', 6, 1, sizex, sizey, -1)
-            self.images1, self.rect1 = load_sprite_sheet('dino_ducking.png', 2, 1, 59, sizey, -1)
-            # self.images1, self.rect1 = load_sprite_sheet('pinkdino_ducking.png', 2, 1, 59, sizey, -1)
+            self.images, self.rect = load_sprite_sheet('dino.png', 
+                                                        6, 1, sizex, sizey, -1)
+            self.images1, self.rect1 = load_sprite_sheet('dino_ducking.png',
+                                                         2, 1, 59, sizey, -1)
         elif type == 'PINK':
-            self.images, self.rect = load_sprite_sheet('pink_dino.png', 6, 1, sizex, sizey, -1)
-            self.images1, self.rect1 = load_sprite_sheet('pink_dino_ducking.png', 2, 1, 59, sizey, -1)
+            self.images, self.rect = load_sprite_sheet('pink_dino.png',
+                                                         6, 1, sizex, sizey, -1)
+            self.images1, self.rect1 = load_sprite_sheet('pink_dino_ducking.png',
+                                                         2, 1, 59, sizey, -1)
         elif type == 'RED':
-            self.images, self.rect = load_sprite_sheet('red_dino.png', 6, 1, sizex, sizey, -1)
-            self.images1, self.rect1 = load_sprite_sheet('red_dino_ducking.png', 2, 1, 59, sizey, -1)    
+            self.images, self.rect = load_sprite_sheet('red_dino.png', 
+                                                        6, 1, sizex, sizey, -1)
+            self.images1, self.rect1 = load_sprite_sheet('red_dino_ducking.png', 
+                                                        2, 1, 59, sizey, -1)    
         elif type == 'ORANGE':
-            self.images, self.rect = load_sprite_sheet('orange_dino.png', 6, 1, sizex, sizey, -1)
-            self.images1, self.rect1 = load_sprite_sheet('orange_dino_ducking.png', 2, 1, 59, sizey, -1) 
+            self.images, self.rect = load_sprite_sheet('orange_dino.png', 
+                                                        6, 1, sizex, sizey, -1)
+            self.images1, self.rect1 = load_sprite_sheet('orange_dino_ducking.png', 
+                                                        2, 1, 59, sizey, -1) 
         elif type == 'YELLOW':
-            self.images, self.rect = load_sprite_sheet('yellow_dino.png', 6, 1, sizex, sizey, -1)
-            self.images1, self.rect1 = load_sprite_sheet('yellow_dino_ducking.png', 2, 1, 59, sizey, -1)
+            self.images, self.rect = load_sprite_sheet('yellow_dino.png', 
+                                                        6, 1, sizex, sizey, -1)
+            self.images1, self.rect1 = load_sprite_sheet('yellow_dino_ducking.png', 
+                                                        2, 1, 59, sizey, -1)
         elif type == 'GREEN':
-            self.images, self.rect = load_sprite_sheet('green_dino.png', 6, 1, sizex, sizey, -1)
-            self.images1, self.rect1 = load_sprite_sheet('green_dino_ducking.png', 2, 1, 59, sizey, -1)
+            self.images, self.rect = load_sprite_sheet('green_dino.png',
+                                                         6, 1, sizex, sizey, -1)
+            self.images1, self.rect1 = load_sprite_sheet('green_dino_ducking.png', 
+                                                        2, 1, 59, sizey, -1)
         elif type == 'PURPLE':
-            self.images, self.rect = load_sprite_sheet('purple_dino.png', 6, 1, sizex, sizey, -1)
-            self.images1, self.rect1 = load_sprite_sheet('purple_dino_ducking.png', 2, 1, 59, sizey, -1)  
+            self.images, self.rect = load_sprite_sheet('purple_dino.png', 
+                                                        6, 1, sizex, sizey, -1)
+            self.images1, self.rect1 = load_sprite_sheet('purple_dino_ducking.png',
+                                                         2, 1, 59, sizey, -1)  
         elif type == 'BLACK':
-            self.images, self.rect = load_sprite_sheet('black_dino.png', 6, 1, sizex, sizey, -1)
-            self.images1, self.rect1 = load_sprite_sheet('black_dino_ducking.png', 2, 1, 59, sizey, -1)    
+            self.images, self.rect = load_sprite_sheet('black_dino.png',
+                                                         6, 1, sizex, sizey, -1)
+            self.images1, self.rect1 = load_sprite_sheet('black_dino_ducking.png',
+                                                         2, 1, 59, sizey, -1)    
         else: 
-            self.images, self.rect = load_sprite_sheet('dino.png', 6, 1, sizex, sizey, -1)
-            self.images1, self.rect1 = load_sprite_sheet('dino_ducking.png', 2, 1, 59, sizey, -1)
-        # 
+            self.images, self.rect = load_sprite_sheet('dino.png', 
+                                                        6, 1, sizex, sizey, -1)
+            self.images1, self.rect1 = load_sprite_sheet('dino_ducking.png', 
+                                                        2, 1, 59, sizey, -1)
 
         self.rect.bottom = int(0.98*height)
         self.rect.left = width/15
@@ -44,73 +58,69 @@ class Dino():
         self.index = 0
         self.counter = 0
         self.score = 0
-        self.isJumping = False
-        self.isDead = False
-        self.isDucking = False
-        self.isBlinking = False
+        self.is_jumping = False
+        self.is_dead = False
+        self.is_ducking = False
+        self.is_blinking = False
         self.movement = [0, 0]
-        self.jumpSpeed = 11.5
-        self.superJumpSpeed = self.jumpSpeed * 1.3
+        self.jump_speed = 11.5
+        self.super_jump_speed = self.jumpSpeed * 1.3
         self.collision_immune = False
-        self.isSuper = False
-
-        self.stand_pos_width = self.rect.width
-        self.duck_pos_width = self.rect1.width
+        self.is_super = False
+        self.stand_width = self.rect.width
+        self.duck_width = self.rect1.width
 
     def draw(self):
         screen.blit(self.image,self.rect)
 
-    ## 충돌 판단 ##
-    def checkbounds(self):
+    #충돌판단
+    def check_bounds(self):
         if self.rect.bottom > int(0.98*height):
             self.rect.bottom = int(0.98*height)
-            self.isJumping = False
+            self.is_jumping = False
 
     def update(self):
-        if self.isJumping:
+        if self.is_jumping:
             self.movement[1] = self.movement[1] + gravity
 
-        if self.isJumping:
+        if self.is_jumping:
             self.index = 0
-        elif self.isBlinking:
+        elif self.is_blinking:
             if self.index == 0:
                 if self.counter % 400 == 399:
                     self.index = (self.index + 1)%2
             else:
                 if self.counter % 20 == 19:
                     self.index = (self.index + 1)%2
-
-        elif self.isDucking:
+        elif self.is_ducking:
             if self.counter % 5 == 0:
                 self.index = (self.index + 1)%2
         else:
             if self.counter % 5 == 0:
                 self.index = (self.index + 1)%2 + 2
 
-        if self.isDead:
+        if self.is_dead:
             self.index = 4
 
         if self.collision_immune:
             if self.counter % 10 == 0:
                 self.index = 5
 
-        if not self.isDucking:
+        if not self.is_ducking:
             self.image = self.images[self.index]
-            self.rect.width = self.stand_pos_width
+            self.rect.width = self.stand_width
         else:
             self.image = self.images1[self.index % 2]
             if self.collision_immune is True:
                 if self.counter % 5 == 0:
                     self.image = self.images[5]
-            self.rect.width = self.duck_pos_width
+            self.rect.width = self.duck_width
 
         self.rect = self.rect.move(self.movement)
-        self.checkbounds()
-
-        if not self.isDead and self.counter % 7 == 6 and self.isBlinking == False:
+        self.check_bounds()
+        if not self.is_dead and self.counter % 7 == 6 and not self.is_blinking :
             self.score += 1
             if self.score % 100 == 0 and self.score != 0:
-                if pygame.mixer.get_init() != None:
-                    checkPoint_sound.play()
-
+                if mixer.get_init() is not None:
+                    check_point_sound.play()
         self.counter = (self.counter + 1)
--- src/setting.py
+++ src/setting.py
@@ -1,186 +1,176 @@
 import os
 import sys
 import random
+from pygame.locals import RESIZABLE,RLEACCEL
 import pygame
-from pygame import *
+from pygame import mixer
+from pygame import display
+from pygame import time
+from pygame import transform
+from pygame import Rect
+from pygame import Surface
 
-
-pygame.mixer.pre_init(44100, -16, 2, 2048)
+mixer.pre_init(44100, -16, 2, 2048)
 pygame.init()
-gamername=''
+display.set_caption("Milk Dragon's Adventure by_MilkDragon")
+gamer_name=''
 scr_size = (width, height) = (800, 400)
 FPS = 60
 gravity = 0.65
 font = pygame.font.Font('DungGeunMo.ttf', 32)
 full_screen=False
-monitor_size = (monitor_width, monitor_height) = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-
+monitor_size = (monitor_width, monitor_height) = (display.Info().current_w, 
+                                                display.Info().current_h)
+background_col = (235,235,235)
 black = (0,0,0)
 white = (255,255,255)
-background_col = (235,235,235)
 green = (0,200,0)
 orange = (255,127,0)
 blue = (0,0,225)
 bright_red = (255,0,0)
 bright_green = (0,255,0)
 bright_orange = (255,215,0)
-
 high_score = 0
-resized_screen = pygame.display.set_mode((scr_size), RESIZABLE)
+resized_screen = display.set_mode((scr_size), RESIZABLE)
 screen = resized_screen.copy()
-resized_screen_centerpos = (0,0)
-rwidth = resized_screen.get_width()
-rheight = resized_screen.get_height()
+resized_screen_center = (0,0)
+r_width = resized_screen.get_width()
+r_height = resized_screen.get_height()
 button_offset = 0.18
-
-clock = pygame.time.Clock()
-pygame.display.set_caption("Milk Dragon's Adventure by_MilkDragon")
-
-bgm_on=True
+clock = time.Clock()
 on_pushtime=0
 off_pushtime=0
-jump_sound = pygame.mixer.Sound('sprites/jump.wav')
-die_sound = pygame.mixer.Sound('sprites/die.wav')
-checkPoint_sound = pygame.mixer.Sound('sprites/checkPoint.wav')
-#background_music = pygame.mixer.Sound('sprites/t-rex_bgm1.mp3')
-
-# HERE: REMOVE SOUND!!
-# pygame.mixer.music.load('sprites/t-rex_bgm1.mp3')
-
-
 dino_size = [44, 47]
 object_size = [40, 40]
 ptera_size = [46, 40]
 collision_immune_time = 500
 shield_time = 2000
-speed_up_limit_count = 700
+speed_up_limit = 700
+bgm_on=True
+jump_sound = mixer.Sound('sprites/jump.wav')
+die_sound = mixer.Sound('sprites/die.wav')
+check_point_sound = mixer.Sound('sprites/checkPoint.wav')
+#background_music = pygame.mixer.Sound('sprites/t-rex_bgm1.mp3')
+# HERE: REMOVE SOUND!!
+# pygame.mixer.music.load('sprites/t-rex_bgm1.mp3')
+
+
 
 # 게임 내에 text를 넣을때 쓰는 함수
-def draw_text(text,font,surface,x,y,main_color) :
-    text_obj = font.render(text,True,main_color)
+def draw_text(text, font, surface, x, y, main_color) :
+    text_obj = font.render(text, True, main_color)
     text_rect = text_obj.get_rect()
     text_rect.centerx = x
     text_rect.centery = y
-    surface.blit(text_obj,text_rect)
+    surface.blit(text_obj, text_rect)
+
 
 def text_objects(text, font):
-    textSurface = font.render(text, True, (black))
-    return textSurface, textSurface.get_rect()
+    text_surface = font.render(text, True, (black))
+    return text_surface, text_surface.get_rect()
+
 
 # 게임 내 image를 넣을 때 쓰는 함수
-def load_image(
-    name,
-    sizex=-1,
-    sizey=-1,
-    colorkey=None,
-    ):
-
-    fullname = os.path.join('sprites', name)
-    image = pygame.image.load(fullname)
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey is -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey, RLEACCEL)
-
+def load_image(name, sizex=-1, sizey=-1, color_key=None):
+    full_name = os.path.join('sprites', name)
+    img = pygame.image.load(full_name)
+    img = img.convert()
+    if color_key is not None:
+        if color_key is -1:
+            color_key = img.get_at((0, 0))
+        img.set_colorkey(color_key, RLEACCEL)
     if sizex != -1 or sizey != -1:
-        image = pygame.transform.scale(image, (sizex, sizey))
+        img= transform.scale(img, (sizex, sizey))
+    return (img, img.get_rect())
 
-    return (image, image.get_rect())
 
-def load_sprite_sheet(
-        sheetname,
-        nx,
-        ny,
-        scalex = -1,
-        scaley = -1,
-        colorkey = None,
-        ):
-    fullname = os.path.join('sprites', sheetname)
-    sheet = pygame.image.load(fullname)
+def load_sprite_sheet(sheet_name, nx, ny, 
+                    scalex=-1, scaley=-1, color_key=None):
+    full_name = os.path.join('sprites', sheet_name)
+    sheet = pygame.image.load(full_name)
     sheet = sheet.convert()
-
     sheet_rect = sheet.get_rect()
-
     sprites = []
-
     sizex = sheet_rect.width/nx
     sizey = sheet_rect.height/ny
-
-    for i in range(0,ny):
-        for j in range(0,nx):
-            rect = pygame.Rect((j*sizex,i*sizey,sizex,sizey))
-            image = pygame.Surface(rect.size)
-            image = image.convert()
-            image.blit(sheet,(0,0),rect)
-
-            if colorkey is not None:
-                if colorkey is -1:
-                    colorkey = image.get_at((0,0))
-                image.set_colorkey(colorkey,RLEACCEL)
-
+    for i in range(0, ny):
+        for j in range(0, nx):
+            rect = Rect((j*sizex, i*sizey, sizex, sizey))
+            img = Surface(rect.size)
+            img = img.convert()
+            img.blit(sheet,(0,0),rect)
+            if color_key is not None:
+                if color_key is -1:
+                    color_key = img.get_at((0,0))
+                img.set_colorkey(color_key, RLEACCEL)
             if scalex != -1 or scaley != -1:
-                image = pygame.transform.scale(image,(scalex,scaley))
-
-            sprites.append(image)
-
+                img = transform.scale(img,(scalex,scaley))
+            sprites.append(img)
     sprite_rect = sprites[0].get_rect()
-
     return sprites, sprite_rect
 
-def disp_gameOver_msg(gameover_image):
 
-    gameover_rect = gameover_image.get_rect()
+def disp_gameover_msg(gameover_img):
+    gameover_rect = gameover_img.get_rect()
     gameover_rect.centerx = width / 2
     gameover_rect.centery = height*0.35
+    screen.blit(gameover_img, gameover_rect)
 
-    screen.blit(gameover_image, gameover_rect)
 
-def disp_intro_buttons(btn_gamestart, btn_board, btn_option):
-    btn_gamestart_rect = btn_gamestart.get_rect()
+def disp_intro_buttons(btn_start, btn_board, btn_option):
+    btn_start_rect = btn_start.get_rect()
     btn_board_rect = btn_board.get_rect()
     btn_option_rect = btn_option.get_rect()
-
-    btn_gamestart_rect.centerx, btn_board_rect.centerx, btn_option_rect.centerx = width * 0.72, width * 0.72, width * 0.72
-    btn_gamestart_rect.centery, btn_board_rect.centery, btn_option_rect.centery = height * 0.5, height * (0.5+button_offset), height * (0.5+2*button_offset)
-    
-    screen.blit(btn_gamestart, btn_gamestart_rect)
+    btn_start_rect.centerx = width * 0.72
+    btn_board_rect.centerx = width * 0.72
+    btn_option_rect.centerx = width * 0.72
+    btn_start_rect.centery = height * 0.5, 
+    btn_board_rect.centery =  height * (0.5 + button_offset)
+    btn_option_rect.centery = height * (0.5 + 2*button_offset)
+    screen.blit(btn_start, btn_start_rect)
     screen.blit(btn_board, btn_board_rect)
     screen.blit(btn_option, btn_option_rect)
 
 
-def checkscrsize(eventw, eventh):
-    if (eventw < width and eventh < height) or eventw < width or eventh < height: #최소해상도
-        resized_screen = pygame.display.set_mode((scr_size), RESIZABLE)
+def check_scr_size(eventw, eventh):
+    if (eventw < width and eventh < height) or (eventw < width) or (eventh < height): 
+        #최소해상도
+        resized_screen = display.set_mode((scr_size), RESIZABLE)
     else:
-        if eventw/eventh!=width/height: #고정화면비
-            adjusted_height=int(eventw/(width/height))
-            resized_screen = pygame.display.set_mode((eventw,adjusted_height), RESIZABLE)
+        if (eventw / eventh) != (width / height): 
+            #고정화면비
+            adjusted_height=int(eventw / (width / height))
+            resized_screen = display.set_mode((eventw, adjusted_height), RESIZABLE)
+
 
 def full_screen_issue():
     global scr_size
-    resized_screen = pygame.display.set_mode((scr_size), RESIZABLE)
-    resized_screen = pygame.display.set_mode((scr_size), RESIZABLE)
+    resized_screen = display.set_mode((scr_size), RESIZABLE)
+    resized_screen = display.set_mode((scr_size), RESIZABLE)
 
-def extractDigits(number):
+
+def extract_digits(number):
     if number > -1:
         digits = []
         i = 0
-        while(number/10 != 0):
-            digits.append(number%10)
-            number = int(number/10)
-
-        digits.append(number%10)
-        for i in range(len(digits),5):
+        while((number / 10) != 0):
+            digits.append(number % 10)
+            number = int(number / 10)
+        digits.append(number % 10)
+        for i in range(len(digits), 5):
             digits.append(0)
         digits.reverse()
         return digits
 
+
 def resize(name, w, h, color):
         global width, height, resized_screen
-        print("resized_screen: (",resized_screen.get_width(),",",resized_screen.get_height(),")")
-        return (name, w*resized_screen.get_width()//width, h*resized_screen.get_height()//height, color)
+        print("resized_screen: (", resized_screen.get_width(), 
+            ",", resized_screen.get_height(), ")")
+        return (name, w * resized_screen.get_width() // width, 
+                h * resized_screen.get_height() // height, color)
 
-def textsize(size):
+
+def text_size(size):
     font = pygame.font.Font('DungGeunMo.ttf', size)
     return font
