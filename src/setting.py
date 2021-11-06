de14c169095097fe0eadf29fac96eafdca274425
MINJU <alswn9938@gmail.com>
2021-11-06 18:27:08 +0900

    [21.11.06 정민주][Add] store 페이지

--- src/setting.py
+++ src/setting.py
@@ -37,6 +37,7 @@ resized_screen_center = (0, 0)
 r_width = resized_screen.get_width()
 r_height = resized_screen.get_height()
 button_offset = 0.2
+width_offset = 0.3
 clock = time.Clock()
 on_pushtime = 0
 off_pushtime = 0
@@ -119,6 +120,21 @@ def disp_gameover_msg(gameover_img):
     gameover_rect.centery = height * 0.35
     screen.blit(gameover_img, gameover_rect)
 
+def disp_store_buttons(btn_restart, btn_save, btn_exit):
+    btn_restart_rect = btn_restart.get_rect()
+    btn_save_rect = btn_save.get_rect()
+    btn_exit_rect = btn_exit.get_rect()
+    btn_restart_rect.centerx = width * 0.2
+    btn_save_rect.centerx = width * (0.2 + width_offset)
+    btn_exit_rect.centerx = width * (0.2 + 2 * width_offset)
+    btn_restart_rect.centery = height * 0.5
+    btn_save_rect.centery = height * 0.5
+    btn_exit_rect.centery = height * 0.5
+    screen.blit(btn_restart, btn_restart_rect)
+    screen.blit(btn_save, btn_save_rect)
+    screen.blit(btn_exit, btn_exit_rect)
+
+
 def disp_gameover_buttons(btn_restart, btn_save, btn_exit):
     btn_restart_rect = btn_restart.get_rect()
     btn_save_rect = btn_save.get_rect()
@@ -126,12 +142,13 @@ def disp_gameover_buttons(btn_restart, btn_save, btn_exit):
     btn_restart_rect.centerx = width * 0.25
     btn_save_rect.centerx = width * 0.5
     btn_exit_rect.centerx = width * 0.75
-    btn_restart_rect.centery = height * 0.5
-    btn_save_rect.centery = height * 0.5
-    btn_exit_rect.centery = height * 0.5
+    btn_restart_rect.centery = height * 0.6
+    btn_save_rect.centery = height * 0.6
+    btn_exit_rect.centery = height * 0.6
     screen.blit(btn_restart, btn_restart_rect)
     screen.blit(btn_save, btn_save_rect)
     screen.blit(btn_exit, btn_exit_rect)
+
 
 def disp_intro_buttons(btn_start, btn_board, btn_option):
     btn_start_rect = btn_start.get_rect()
--- src/store.py
+++ src/store.py
@@ -1,4 +1,3 @@
-from pygame.image import load
 from src.item import *
 from src.interface import *
 from db.db_interface import InterfDB
@@ -12,6 +11,12 @@ def store():
     #배경 이미지
     back_store, back_store_rect = load_image('coin_t_rex3.png', width, height)
     #버튼 이미지
+    char_btn_image, char_btn_rect = load_image('character.png', 150, 80,-1)
+    r_char_btn_image, r_char_btn_rect = load_image(*resize('character.png', 150, 80,-1))
+    skin_btn_image, skin_btn_rect = load_image('skin.png', 150, 80,-1)
+    r_skin_btn_image, r_skin_btn_rect = load_image(*resize('skin.png', 150, 80,-1))
+    item_btn_image, item_btn_rect = load_image('item_btn.png', 150, 80,-1)
+    r_item_btn_image, r_item_btn_rect = load_image(*resize('item_btn.png', 150, 80,-1))
 
     while not game_start:
         for event in pygame.event.get():
@@ -21,7 +26,27 @@ def store():
                 check_scr_size(event.w, event.h)
             if event.type == pygame.QUIT:
                 game_start = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     if pygame.mouse.get_pressed() == (1, 0, 0):
+            #         x, y = event.pos
+            #         if r_char_btn_rect.collidepoint(x, y):
+            #             #gameplay_easy()
+            #         if r_skin_btn_rect.collidepoint(x, y):
+            #             #gameplay_hard()
+            #         if r_item_btn_rect.collidepoint(x, y):
+            #             #store()
+
+        r_char_btn_rect.centerx = resized_screen.get_width() * 0.2
+        r_char_btn_rect.centery = resized_screen.get_height() * 0.5
+        r_skin_btn_rect.centerx = resized_screen.get_width() * (0.2 + width_offset)
+        r_skin_btn_rect.centery = resized_screen.get_height() * 0.5
+        r_item_btn_rect.centerx = resized_screen.get_width() * (0.2 + 2 * width_offset)
+        r_item_btn_rect.centery = resized_screen.get_height() * 0.5
         screen.blit(back_store, back_store_rect)
+        disp_store_buttons(char_btn_image, skin_btn_image, item_btn_image)
         resized_screen.blit(
             pygame.transform.scale(screen, (resized_screen.get_width(), resized_screen.get_height())),
             resized_screen_center)
