cb5c142b1b20b8f432390f54d6fe2dcaf9a9443f
MINJU <alswn9938@gmail.com>
2021-11-11 09:57:51 +0900

    [21.11.11 정민주] store 화면 구성 완료

--- src/store.py
+++ src/store.py
@@ -4,6 +4,13 @@ from db.db_interface import InterfDB
 db = InterfDB("db/score.db")
 import src.game
 
+global btn_offset
+global item_price_offset
+global item_btn_offset
+btn_offset = 0.2
+item_price_offset = 0.18
+item_btn_offset = 0.28
+
 def store():
     global resized_screen
     game_start = False
@@ -34,10 +41,10 @@ def store():
             if event.type == pygame.MOUSEBUTTONDOWN:
                  if pygame.mouse.get_pressed() == (1, 0, 0):
                      x, y = event.pos
-            #         if r_char_btn_rect.collidepoint(x, y):
-            #             #gameplay_easy()
-            #         if r_skin_btn_rect.collidepoint(x, y):
-            #             #gameplay_hard()
+                     if r_char_btn_rect.collidepoint(x, y):
+                        char_store()
+                     if r_skin_btn_rect.collidepoint(x, y):
+                         skin_store()
                      if r_item_btn_rect.collidepoint(x, y):
                          item_store()
                      if r_back_btn_rect.collidepoint(x, y):
@@ -61,13 +68,11 @@ def store():
     pygame.quit()
     quit()
 
+
 def item_store():
     global resized_screen
     image_space = 0.3
     game_start = False
-    btn_offset = 0.25
-    item_price_offset = 0.18
-    item_btn_offset = 0.28
     # 배경 이미지
     back_store, back_store_rect = load_image('coin_t_rex3.png', width, height)
     # 코인 이미지
@@ -214,7 +219,6 @@ def item_store():
         screen.blit(user_coin_image, user_coin_rect)
         screen.blit(user_coin, user_c_rect)
 
-
         if(coin_item_count >= s_price):
             screen.blit(buy_btn1_image, buy_btn1_rect)
         else:
@@ -238,3 +242,307 @@ def item_store():
         clock.tick(FPS)
     pygame.quit()
     quit()
+
+
+def char_store():
+    global resized_screen
+    image_space = 0.3
+    game_start = False
+    # 배경 이미지
+    back_store, back_store_rect = load_image('coin_t_rex3.png', width, height)
+    # 코인 이미지
+    coin1_image, _ = load_sprite_sheet('coin.png', 1, 7, -1, -1, -1)
+    coin1_image = transform.scale(coin1_image[0], (25, 25))
+    coin1_rect = coin1_image.get_rect()
+    coin2_image, _ = load_sprite_sheet('coin.png', 1, 7, -1, -1, -1)
+    coin2_image = transform.scale(coin2_image[0], (25, 25))
+    coin2_rect = coin2_image.get_rect()
+    coin3_image, _ = load_sprite_sheet('coin.png', 1, 7, -1, -1, -1)
+    coin3_image = transform.scale(coin3_image[0], (25, 25))
+    coin3_rect = coin3_image.get_rect()
+    coin4_image, _ = load_sprite_sheet('coin.png', 1, 7, -1, -1, -1)
+    coin4_image = transform.scale(coin4_image[0], (25, 25))
+    coin4_rect = coin4_image.get_rect()
+    # 아이템 이미지
+    purple_image, _ = load_sprite_sheet('purple_dino.png', 6, 1, -1, -1, -1)
+    purple_image = transform.scale(purple_image[0], (60, 60))
+    purple_rect = purple_image.get_rect()
+    red_image, _ = load_sprite_sheet('red_dino.png', 6, 1, -1, -1, -1)
+    red_image = transform.scale(red_image[0], (60, 60))
+    red_rect = red_image.get_rect()
+    yellow_image, _ = load_sprite_sheet('yellow_dino.png', 6, 1, -1, -1, -1)
+    yellow_image = transform.scale(yellow_image[0], (60, 60))
+    yellow_rect = yellow_image.get_rect()
+    tux_image, _ = load_sprite_sheet('tux.png', 8, 9, -1, -1, -1)
+    tux_image = transform.scale(tux_image[0], (60, 60))
+    tux_rect = tux_image.get_rect()
+    # user의 코인
+    coin_item_count = db.query_db("select coin from item where item_id=1;", one=True)['coin']
+    user_coin_image, _ = load_sprite_sheet('coin.png', 1, 7, -1, -1, -1)
+    user_coin_image = transform.scale(user_coin_image[0], (20, 20))
+    user_coin_rect = user_coin_image.get_rect()
+    user_font = pygame.font.Font('DungGeunMo.ttf', 16)
+    user_coin = user_font.render(f'X {coin_item_count}', True, black)
+    # skin store 버튼 이미지
+    buy_btn1_image, buy_btn1_rect = load_image('buy.png', 100, 50, -1)
+    buy_btn2_image, buy_btn2_rect = load_image('buy.png', 100, 50, -1)
+    buy_btn3_image, buy_btn3_rect = load_image('buy.png', 100, 50, -1)
+    buy_btn4_image, buy_btn4_rect = load_image('buy.png', 100, 50, -1)
+    no_money1_image, no_money1_rect = load_image('X.png', 100, 50, -1)
+    no_money2_image, no_money2_rect = load_image('X.png', 100, 50, -1)
+    no_money3_image, no_money3_rect = load_image('X.png', 100, 50, -1)
+    no_money4_image, no_money4_rect = load_image('X.png', 100, 50, -1)
+    sold_out1_image, sold_out1_rect = load_image('sold_out.png', 100, 50, -1)
+    sold_out2_image, sold_out2_rect = load_image('sold_out.png', 100, 50, -1)
+    sold_out3_image, sold_out3_rect = load_image('sold_out.png', 100, 50, -1)
+    sold_out4_image, sold_out4_rect = load_image('sold_out.png', 100, 50, -1)
+    # 뒤로 가기 버튼 이미지
+    back_btn_image, back_btn_rect = load_image('btn_back.png', 100, 50, -1)
+    r_back_btn_image, r_back_btn_rect = load_image(*resize('btn_back.png', 100, 50, -1))
+    # 아이템
+
+    # 폰트
+    p_price, r_price, y_price, t_price = 1, 1, 1, 1
+    my_font = pygame.font.Font('DungGeunMo.ttf', 18)
+    purple_price = my_font.render(f"x {p_price}", True, black)
+    red_price = my_font.render(f'x {r_price}', True, black)
+    yellow_price = my_font.render(f'x {y_price}', True, black)
+    tux_price = my_font.render(f'x {y_price}', True, black)
+    # 배치
+    (purple_rect.centerx, purple_rect.centery) = (width * 0.2, height * 0.37)
+    (coin1_rect.centerx, coin1_rect.centery) = (width * 0.18, height * (0.37 + item_price_offset))
+    purple_price_rect = purple_price.get_rect(center=(width * 0.23, height * (0.37 + item_price_offset)))
+    (buy_btn1_rect.centerx, buy_btn1_rect.centery) = (width * 0.2, height * (0.37 + item_btn_offset))
+    (no_money1_rect.centerx, no_money1_rect.centery) = (width * 0.2, height * (0.37 + item_btn_offset))
+    #
+    (red_rect.centerx, red_rect.centery) = (width * (0.2 + btn_offset), height * 0.37)
+    (coin2_rect.centerx, coin2_rect.centery) = (width * (0.18 + btn_offset), height * (0.37 + item_price_offset))
+    red_price_rect = red_price.get_rect(center=(width * (0.23 + btn_offset), height * (0.37 + item_price_offset)))
+    (buy_btn2_rect.centerx, buy_btn2_rect.centery) = (width * (0.2 + btn_offset), height * (0.37 + item_btn_offset))
+    (no_money2_rect.centerx, no_money2_rect.centery) = (width * (0.2 + btn_offset), height * (0.37 + item_btn_offset))
+    #
+    (yellow_rect.centerx, yellow_rect.centery) = (width * (0.2 + 2 * btn_offset), height * 0.37)
+    (coin3_rect.centerx, coin3_rect.centery) = (width * (0.18 + 2 * btn_offset), height * (0.37 + item_price_offset))
+    yellow_price_rect = yellow_price.get_rect(center=(width * (0.23 + 2 * btn_offset), height * (0.37 + item_price_offset)))
+    (buy_btn3_rect.centerx, buy_btn3_rect.centery) = (width * (0.2 + 2 * btn_offset), height * (0.37 + item_btn_offset))
+    (no_money3_rect.centerx, no_money3_rect.centery) = (width * (0.2 + 2 * btn_offset), height * (0.37 + item_btn_offset))
+    #
+    (tux_rect.centerx, tux_rect.centery) = (width * (0.2 + 3 * btn_offset), height * 0.37)
+    (coin4_rect.centerx, coin4_rect.centery) = (width * (0.18 + 3 * btn_offset), height * (0.37 + item_price_offset))
+    tux_price_rect = tux_price.get_rect(center=(width * (0.23 + 3 * btn_offset), height * (0.37 + item_price_offset)))
+    (buy_btn4_rect.centerx, buy_btn4_rect.centery) = (width * (0.2 + 3 * btn_offset), height * (0.37 + item_btn_offset))
+    (no_money4_rect.centerx, no_money4_rect.centery) = (width * (0.2 + 3 * btn_offset), height * (0.37 + item_btn_offset))
+    #
+    r_back_btn_rect.centerx = resized_screen.get_width() * 0.1
+    r_back_btn_rect.centery = resized_screen.get_height() * 0.1
+    #
+    (user_coin_rect.centerx, user_coin_rect.centery) = (width * (0.65 + btn_offset), height * 0.08)
+    user_c_rect = user_coin.get_rect(center=(width * (0.69 + btn_offset), height * 0.08))
+
+    while not game_start:
+        for event in pygame.event.get():
+            if event.type == pygame.VIDEORESIZE and not full_screen:
+                back_store_rect.bottomleft = (width * 0, height)
+            if event.type == pygame.VIDEORESIZE:
+                check_scr_size(event.w, event.h)
+            if event.type == pygame.QUIT:
+                game_start = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+            if pygame.mouse.get_pressed() == (1, 0, 0):
+                x, y = event.pos
+                if r_back_btn_rect.collidepoint(x, y):
+                    store()
+                # if buy_btn1_rect.collidepoint(x, y) and coin_item_count >= s_price:
+                #     db.query_db(
+                #         f"update item set shield = {shield_item_count + 1},  coin = {coin_item_count - s_price} where item_id=1;")
+                #     db.commit()
+                # if buy_btn2_rect.collidepoint(x, y) and coin_item_count >= f_price:
+                #     db.query_db(
+                #         f"update item set life = {life_item_count + 1},  coin = {coin_item_count - l_price} where item_id=1;")
+                #     db.commit()
+                # if buy_btn3_rect.collidepoint(x, y) and coin_item_count >= t_price:
+                #     db.query_db(
+                #         f"update item set slow = {slow_item_count + 1},  coin = {coin_item_count - t_price} where item_id=1;")
+                #     db.commit()
+
+        screen.blit(back_store, back_store_rect)
+        screen.blit(coin1_image, coin1_rect)
+        screen.blit(coin2_image, coin2_rect)
+        screen.blit(coin3_image, coin3_rect)
+        screen.blit(coin4_image, coin4_rect)
+        screen.blit(purple_image, purple_rect)
+        screen.blit(red_image, red_rect)
+        screen.blit(yellow_image, yellow_rect)
+        screen.blit(tux_image, tux_rect)
+        screen.blit(purple_price, purple_price_rect)
+        screen.blit(red_price, red_price_rect)
+        screen.blit(yellow_price, yellow_price_rect)
+        screen.blit(tux_price, tux_price_rect)
+        if (coin_item_count >= p_price):
+            screen.blit(buy_btn1_image, buy_btn1_rect)
+        else:
+            screen.blit(no_money1_image, no_money1_rect)
+        if (coin_item_count >= r_price):
+            screen.blit(buy_btn2_image, buy_btn2_rect)
+        else:
+            screen.blit(no_money2_image, no_money2_rect)
+        if (coin_item_count >= y_price):
+            screen.blit(buy_btn3_image, buy_btn3_rect)
+        else:
+            screen.blit(no_money3_image, no_money3_rect)
+        if (coin_item_count >= t_price):
+            screen.blit(buy_btn4_image, buy_btn4_rect)
+        else:
+            screen.blit(no_money4_image, no_money4_rect)
+        # sold out 버튼 구현 필요
+        screen.blit(back_btn_image, back_btn_rect)
+        screen.blit(user_coin, user_c_rect)
+        screen.blit(user_coin_image, user_coin_rect)
+        resized_screen.blit(
+            pygame.transform.scale(screen, (resized_screen.get_width(), resized_screen.get_height())),
+            resized_screen_center)
+        pygame.display.update()
+        clock.tick(FPS)
+    pygame.quit()
+    quit()
+
+
+def skin_store():
+    global resized_screen
+    image_space = 0.3
+    game_start = False
+    # 배경 이미지
+    back_store, back_store_rect = load_image('coin_t_rex3.png', width, height)
+    # 코인 이미지
+    coin1_image, _ = load_sprite_sheet('coin.png', 1, 7, -1, -1, -1)
+    coin1_image = transform.scale(coin1_image[0], (25, 25))
+    coin1_rect = coin1_image.get_rect()
+    coin2_image, _ = load_sprite_sheet('coin.png', 1, 7, -1, -1, -1)
+    coin2_image = transform.scale(coin2_image[0], (25, 25))
+    coin2_rect = coin2_image.get_rect()
+    coin3_image, _ = load_sprite_sheet('coin.png', 1, 7, -1, -1, -1)
+    coin3_image = transform.scale(coin3_image[0], (25, 25))
+    coin3_rect = coin3_image.get_rect()
+    # 아이템 이미지
+    spring_image, spring_rect = load_image('ex_spring.png', 300, 280, -1)
+    fall_image, fall_rect = load_image('ex_fall.png', 300, 280, -1)
+    winter_image, winter_rect = load_image('ex_winter.png', 300, 280, -1)
+    #user의 코인
+    coin_item_count = db.query_db("select coin from item where item_id=1;", one=True)['coin']
+    user_coin_image, _ = load_sprite_sheet('coin.png', 1, 7, -1, -1, -1)
+    user_coin_image = transform.scale(user_coin_image[0], (20, 20))
+    user_coin_rect = user_coin_image.get_rect()
+    user_font = pygame.font.Font('DungGeunMo.ttf', 16)
+    user_coin = user_font.render(f'X {coin_item_count}', True, black)
+    # skin store 버튼 이미지
+    buy_btn1_image, buy_btn1_rect = load_image('buy.png', 100, 50, -1)
+    buy_btn2_image, buy_btn2_rect = load_image('buy.png', 100, 50, -1)
+    buy_btn3_image, buy_btn3_rect = load_image('buy.png', 100, 50, -1)
+    no_money1_image, no_money1_rect = load_image('X.png', 100, 50, -1)
+    no_money2_image, no_money2_rect = load_image('X.png', 100, 50, -1)
+    no_money3_image, no_money3_rect = load_image('X.png', 100, 50, -1)
+    sold_out1_image, sold_out1_rect = load_image('sold_out.png', 100, 50, -1)
+    sold_out2_image, sold_out2_rect = load_image('sold_out.png', 100,50, -1)
+    sold_out3_image, sold_out3_rect = load_image('sold_out.png', 100, 50, -1)
+    # 뒤로 가기 버튼 이미지
+    back_btn_image, back_btn_rect = load_image('btn_back.png', 100, 50, -1)
+    r_back_btn_image, r_back_btn_rect = load_image(*resize('btn_back.png', 100, 50, -1))
+    # 아이템
+
+    # 폰트
+    s_price, f_price, w_price = 1, 1, 1
+    my_font = pygame.font.Font('DungGeunMo.ttf', 18)
+    spring_price = my_font.render(f"x {s_price}", True, black)
+    fall_price = my_font.render(f'x {f_price}', True, black)
+    winter_price = my_font.render(f'x {w_price}', True, black)
+    # 배치
+    (spring_rect.centerx, spring_rect.centery) = (width * 0.25, height * 0.37)
+    (coin1_rect.centerx, coin1_rect.centery) = (width * 0.23, height * (0.37 + item_price_offset))
+    spring_price_rect = spring_price.get_rect(center=(width * 0.28, height * (0.37 + item_price_offset)))
+    (buy_btn1_rect.centerx, buy_btn1_rect.centery) = (width * 0.25, height * (0.37 + item_btn_offset))
+    (no_money1_rect.centerx, no_money1_rect.centery) = (width * 0.25, height * (0.37 + item_btn_offset))
+    #
+    (fall_rect.centerx, fall_rect.centery) = (width * (0.25 + btn_offset), height * 0.37)
+    (coin2_rect.centerx, coin2_rect.centery) = (width * (0.23 + btn_offset), height * (0.37 + item_price_offset))
+    fall_price_rect = fall_price.get_rect(center=(width * (0.28 + btn_offset), height * (0.37 + item_price_offset)))
+    (buy_btn2_rect.centerx, buy_btn2_rect.centery) = (width * (0.25 + btn_offset), height * (0.37 + item_btn_offset))
+    (no_money2_rect.centerx, no_money2_rect.centery) = (width * (0.25 + btn_offset), height * (0.37 + item_btn_offset))
+    #
+    (winter_rect.centerx, winter_rect.centery) = (width * (0.25 + 2 * btn_offset), height * 0.37)
+    (coin3_rect.centerx, coin3_rect.centery) = (width * (0.23 + 2 * btn_offset), height * (0.37 + item_price_offset))
+    winter_price_rect = winter_price.get_rect(center=(width * (0.28 + 2 * btn_offset), height * (0.37 + item_price_offset)))
+    (buy_btn3_rect.centerx, buy_btn3_rect.centery) = (width * (0.25 + 2 * btn_offset), height * (0.37 + item_btn_offset))
+    (no_money3_rect.centerx, no_money3_rect.centery) = (width * (0.25 + 2 * btn_offset), height * (0.37 + item_btn_offset))
+    #
+    r_back_btn_rect.centerx = resized_screen.get_width() * 0.1
+    r_back_btn_rect.centery = resized_screen.get_height() * 0.1
+    #
+    (user_coin_rect.centerx, user_coin_rect.centery) = (width * (0.65 + btn_offset), height * 0.08)
+    user_c_rect = user_coin.get_rect(center=(width * (0.69 + btn_offset ), height * 0.08))
+
+
+    while not game_start:
+        for event in pygame.event.get():
+            if event.type == pygame.VIDEORESIZE and not full_screen:
+                back_store_rect.bottomleft = (width * 0, height)
+            if event.type == pygame.VIDEORESIZE:
+                check_scr_size(event.w, event.h)
+            if event.type == pygame.QUIT:
+                game_start = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+            if pygame.mouse.get_pressed() == (1, 0, 0):
+                x, y = event.pos
+                if r_back_btn_rect.collidepoint(x, y):
+                    store()
+                # if buy_btn1_rect.collidepoint(x, y) and coin_item_count >= s_price:
+                #     db.query_db(
+                #         f"update item set shield = {shield_item_count + 1},  coin = {coin_item_count - s_price} where item_id=1;")
+                #     db.commit()
+                # if buy_btn2_rect.collidepoint(x, y) and coin_item_count >= f_price:
+                #     db.query_db(
+                #         f"update item set life = {life_item_count + 1},  coin = {coin_item_count - l_price} where item_id=1;")
+                #     db.commit()
+                # if buy_btn3_rect.collidepoint(x, y) and coin_item_count >= t_price:
+                #     db.query_db(
+                #         f"update item set slow = {slow_item_count + 1},  coin = {coin_item_count - t_price} where item_id=1;")
+                #     db.commit()
+
+        screen.blit(back_store, back_store_rect)
+        screen.blit(coin1_image, coin1_rect)
+        screen.blit(coin2_image, coin2_rect)
+        screen.blit(coin3_image, coin3_rect)
+        screen.blit(spring_image, spring_rect)
+        screen.blit(fall_image, fall_rect)
+        screen.blit(winter_image, winter_rect)
+        screen.blit(spring_price, spring_price_rect)
+        screen.blit(fall_price, fall_price_rect)
+        screen.blit(winter_price, winter_price_rect)
+        if (coin_item_count >= s_price):
+            screen.blit(buy_btn1_image, buy_btn1_rect)
+        else:
+            screen.blit(no_money1_image, no_money1_rect)
+        if (coin_item_count >= f_price):
+            screen.blit(buy_btn2_image, buy_btn2_rect)
+        else:
+            screen.blit(no_money2_image, no_money2_rect)
+        if (coin_item_count >= w_price):
+            screen.blit(buy_btn3_image, buy_btn3_rect)
+        else:
+            screen.blit(no_money3_image, no_money3_rect)
+        #sold out 버튼 구현 필요
+        screen.blit(back_btn_image, back_btn_rect)
+        screen.blit(user_coin, user_c_rect)
+        screen.blit(user_coin_image, user_coin_rect)
+        resized_screen.blit(
+            pygame.transform.scale(screen, (resized_screen.get_width(), resized_screen.get_height())),
+            resized_screen_center)
+        pygame.display.update()
+        clock.tick(FPS)
+    pygame.quit()
+    quit()
+
+
